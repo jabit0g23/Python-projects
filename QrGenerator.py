e013332aa3b446175f985f1249ff7ef7a3a313c6
jabit0g23 <jabit0g23@gmail.com>
2024-01-07 23:28:43 -0300

    Qr Generator

--- QrGenerator.py
+++ QrGenerator.py
@@ -1,15 +1,26 @@
-import flet as ft
 import qrcode
+from flet import app, Page, TextField, ElevatedButton, Column, Image, ImageFit
 
-def main(page: ft.Page):
-  def btn_click(e):
-    codigo_qr = qrcode.QRCode(
-      version = 1,
-      box_size = 10,
-      border = 4,
-      )
-    codigo_qr = codigo_qr.make_image(fill_color='black', back_color='white')
+def main(page: Page):
+    def btn_click(e):
+        codigo_qr = qrcode.QRCode(
+            version=1,
+            box_size=10,
+            border=4,
+        )
+        codigo_qr.add_data(texto.value)
+        imagen_qr = codigo_qr.make_image(fill_color='black', back_color='white')
+        imagen_qr.save('codigo_qr.png')
 
-  texto = ft.TextField(label='Introduce aqui tu texto')
-  boton = ft.ElevatedButton('Generar')
-  imagen_col = ft.Column(expand=1, wrap=False,scroll='AUTO')
+        imagen_col.controls.append(Image(src='codigo_qr.png', width=400, height=400, fit=ImageFit.CONTAIN))
+        page.update()
+
+    texto = TextField(label='Introduce aquí tu texto')
+    boton = ElevatedButton('Generar')
+    imagen_col = Column(expand=1, wrap=False, scroll='AUTO')
+
+    boton.on_click = btn_click
+
+    page.add(texto, boton, imagen_col)
+
+app(target=main)
